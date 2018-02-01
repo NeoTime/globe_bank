85b8d1d4a492d1f3f27f8a6bd303a18c52b7a045
NeoTime <riglerjeff@gmail.com>
2018-02-01 13:53:53 -0800

    UpdatedWorkandPages

---
+++ public/staff/subjects/show.php
@@ -0,0 +1,16 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
+<?php
+
+// $id = isset($_GET['id']) ? $_GET['id'] : '1';
+$id = $_GET['id'] ?? '1'; // PHP > 7.0
+
+echo h($id);
+
+
+
+?>
+
+<a href="show.php?name=<?php echo u('JohnDoe'); ?>">Link</a><br />
+<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br />
+<a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br />
